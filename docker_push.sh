2f2ba639110cdb596540bbc27c241734d953f94a
basusgs <basu@shrigowri.com>
2018-10-18 10:45:37 +0530

    1212

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t docker build dayanandk/sample:1.0 .
+docker build -t dayanandk/sample:1.0 .
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
 docker push dayanandk/sample:1.0
